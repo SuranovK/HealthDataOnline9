aa0812a2224bbe87230da24e34dcf36fea79dc32
Parahat <prejepow@gmail.com>
2018-07-29 11:51:57 -0500

    invalid search test added

---
+++ src/test/java/com/healthdata/tests/InvalidSearch.java
@@ -0,0 +1,29 @@
+package com.healthdata.tests;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.healthdata.pages.SearchPage;
+
+public class InvalidSearch extends TestBase{
+	
+	@Test (groups = { "smoke" })
+	public void InvalidSearchTermInSearchBox() {
+		
+		SearchPage searchPage = new SearchPage();
+		
+		searchPage.searchInput.sendKeys("c");
+		searchPage.applyButton.click();
+		
+		String actual = searchPage.errorMessage.getText();
+		String expected = "No results were found. Please try another keyword.";
+		
+		assertEquals(actual, expected, "Results does not match");
+		
+		//assertFalse(searchPage.searchResults.isDisplayed());
+	}
+
+}
